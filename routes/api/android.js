79c5164d7062cb9c01b6c531c490ad6cb9b7f76d
TadasLTU <semionovas13@gmail.com>
2019-12-02 22:32:17 +0200

    Update android.js

--- routes/api/android.js
+++ routes/api/android.js
@@ -1,20 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
 
 router.get('/get', function(req, res) 
 {
-	var db = req.db;
-	db.all('SELECT * FROM Matavimai, Ribos ORDER BY ID ASC LIMIT 1;', function(err,matavimas)
-    {
-        if(err)
+    client.connect();
+
+    client.query('SELECT * FROM Matavimai, Ribos ORDER BY ID ASC LIMIT 1;', (err, res) =>
+     {
+        if (err) 
         {
-            console.log('*** Error serving querying database. ' + err);
+            throw err;
         }
-        else
+        for (let row of res.rows)
         {
-			res.json(matavimas);
+          console.log(JSON.stringify(row));
         }
-    });
+        client.end();
+      });
+
+
+
+
+
+
+
+
+
+	//var db = req.db;
+	// db.all('SELECT * FROM Matavimai, Ribos ORDER BY ID ASC LIMIT 1;', function(err,matavimas)
+    // {
+    //     if(err)
+    //     {
+    //         console.log('*** Error serving querying database. ' + err);
+    //     }
+    //     else
+    //     {
+	// 		res.json(matavimas);
+    //     }
+    // });
 });
 
 module.exports = router;
