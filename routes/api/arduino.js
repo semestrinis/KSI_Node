dd483efda14275054685afd721993f8ffa09bd94
TadasLTU <semionovas13@gmail.com>
2019-12-07 22:07:16 +0200

    Update arduino.js

--- routes/api/arduino.js
+++ routes/api/arduino.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 router.get('/', function(req, res) 
 {
     var db = req.db;
