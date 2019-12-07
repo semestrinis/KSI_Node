f9956ab3b2463b288ad3b80197fd8fc5ac96cbbd
TadasLTU <semionovas13@gmail.com>
2019-12-07 21:22:22 +0200

    PG veikia, valomas kodas

--- routes/api/testPg.js
+++ routes/api/testPg.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var router = express.Router();
-const { Client } = require('pg');
+// var express = require('express');
+// var router = express.Router();
+// const { Client } = require('pg');
 
-router.get('/testGet', function(req, res) 
-{
-	const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-      });
+// router.get('/testGet', function(req, res) 
+// {
+// 	const client = new Client({
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: true,
+//       });
       
-      client.connect();
+//       client.connect();
       
-      client.query('SELECT * FROM Matavimai, Ribos;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          console.log(JSON.stringify(row));
-        }
-        client.end();
-      }); 
-});
+//       client.query('SELECT * FROM Matavimai, Ribos;', (err, res) => {
+//         if (err) throw err;
+//         for (let row of res.rows) {
+//           console.log(JSON.stringify(row));
+//         }
+//         client.end();
+//       }); 
+// });
 
-module.exports = router;
+// module.exports = router;
 //var db = req.db;
 	// db.all('SELECT * FROM Matavimai, Ribos;', function(err,matavimas)
     // {
