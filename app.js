a56945a3b35795a205e4b71c2bcb095fbf943763
TadasLTU <semionovas13@gmail.com>
2019-12-09 23:05:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var debug = require('./routes/api/debug');
 var testPgDb = require('./routes/db');
 var app = express();
 
+
+
 const { Client } = require('pg')
 
 //var sqlite3 = require('sqlite3');
@@ -31,7 +33,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
