6fc90150336794f208e92cbeaa814b4a95825ee1
TadasLTU <semionovas13@gmail.com>
2019-12-10 16:35:31 +0200

    Panaikinti nereikalingi testavimo failai

--- app.js
+++ app.js
@@ -12,44 +12,18 @@ var dataView = require('./routes/dataView');
 var apiAndroid = require('./routes/api/android');
 var apiArduino = require('./routes/api/arduino');
 var debug = require('./routes/api/debug');
-//var testPg = require('./routes/api/testPg');
 var testPgDb = require('./routes/db');
 var app = express();
-
-
-
-const { Client } = require('pg')
-
-//var sqlite3 = require('sqlite3');
-
-/*** db aprasymas ***/
-//var db = new sqlite3.Database('./data/notes.db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-/*** db idejimas i req ***/
-// app.use(function(req, res, next) 
-// {
-//     req.db = db;
-//     next();
-// });
-
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 /*** route aprasymas ***/
 app.use('/', routes);
@@ -58,10 +32,11 @@ app.use('/dataView', dataView);
 app.use('/api/android', apiAndroid);
 app.use('/api/arduino', apiArduino);
 app.use('/api/debug', debug);
-//app.use('/api/testPg', testPg);
 app.use('/db', testPgDb);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+ {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -71,8 +46,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get('env') === 'development')
+{
+  app.use(function(err, req, res, next) 
+  {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -83,7 +60,8 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) 
+{
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
