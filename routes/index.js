d1b9a63ef6fa8afa2796507efe95b8d54b190b29
ssniekcus0 <38216432+ssniekcus0@users.noreply.github.com>
2019-12-10 22:37:52 +0200

    9

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/', async(req, res) =>
       const result1 = await client.query('SELECT * FROM public."Ribos" ORDER BY "ID" DESC LIMIT 1;');
       const results = { 'results': (result) ? result.rows : null};
       const results1 = { 'results1': (result1) ? result1.rows : null};
-      if(result1.Min_Temp > results[0].Temperatura1 && results[0].Temperatura1 < results1[0].Max_Temp)
+      if(result1.Min_Temp > result.Temperatura1 && result.Temperatura1 < result1.Max_Temp)
       {
         rezultatas = "Pats tas";
       }
