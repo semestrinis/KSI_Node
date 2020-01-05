7e120f268e68610efd84605295636da3eacb87ac
NedasZ <zilovas79@gmail.com>
2020-01-05 15:58:10 +0200

    android
    asdasds yes

--- Android/Arduino/app/src/main/java/com/herokuapp/ksi_projektas/arduino/MainActivity.java
+++ Android/Arduino/app/src/main/java/com/herokuapp/ksi_projektas/arduino/MainActivity.java
@@ -176,6 +176,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
             LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{});
+            graph.removeAllSeries();
             for (int i = 0; i < matavimai.length ; i++)
             {
                 series.appendData(new DataPoint(i,matavimai[i].Temperatura1),true,matavimai.length+1);
